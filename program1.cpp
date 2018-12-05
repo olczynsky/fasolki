32ed1d4451da4a1df2c215c5c9170b6b8b609e1f
olczynsky <olczynsky@yahoo.com>
2018-12-05 12:10:40 +0100

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,25 @@
+//ale bedzie kurwa niespodziewanka
+#include <iostream>
+#include <string>
+using namespace std
+;
+
+int main()
+{
+	string cb;
+	string n;
+	int a;
+	string f;
+	string g;
+	cout <<"Czy chcesz poznac odpowiedz na najtrudniejsze pytanie na swiecie? TAK/NIE\n";
+	cin>>cb;
+	cout<<"Ale na pewno? TAK/NIE\n";
+	cin>>n;
+	cout<<"- Jak to jest byc skryba, dobrze?\n";
+	cout<<"Kontynuowac? TAK/NIE\n";
+	cin>>f;
+	cout<<"- Aaa, wie pan, moim zdaniem to nie ma tak, ze dobrze, albo ze niedobrze. Gdybym mial powiedziec, co cenie w zyciu najbardziej, powiedzialbym, ze ludzi. Eeeeem ludzi, ktorzy podali mi pomocna dlon, kiedy sobie nie radzilem, kiedy bylem sam. I co ciekawe, to wlasnie przypadkowe spotkania wplywaja na nasze zycie. Chodzi o to, ze kiedy wyznaje sie pewne wartosci, nawet pozornie uniwersalne, bywa, ze nie znajduje sie zrozumienia, ktore, by tak rzec, ktore pomaga sie nam rozwijac. Ja mialem szczescie, by tak rzec, poniewaz je znalazlem. I dziekuje zyciu. Dziekuje mu, zycie to spiew, zycie to taniec, zycie to milosc. Wielu ludzi pyta mnie o to samo, ale jak ty to robisz? Skad czerpiesz te radosc? A ja odpowiadam, ze to proste, to umilowanie zycia, to wlasnie ono sprawia, ze dzisiaj na przyklad buduje maszyny, a jutro... kto wie, dlaczego by nie, oddam sie pracy spolecznej i bede ot, chodzby sadzic... znaczy... machew.\n"; 
+	cin.get();
+    cin.get();
+	
+}
